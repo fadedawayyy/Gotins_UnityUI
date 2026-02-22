4b8911a76a1a2701bb197a2b6ebd94d6323210e8
fadedawayyy <gergot.gg@gmail.com>
2026-02-22 23:55:35 +0200

    Uztaisiju ScrollView

--- Assets/Scripts/ImageChangeScript.cs
+++ Assets/Scripts/ImageChangeScript.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class ImageChangeScript : MonoBehaviour
 {
     public GameObject imageField;
+    public Text descriptionText; // Add reference to Text UI component
     public Sprite[] soriteArray;
+    public string[] descriptions; // Add array for descriptions
 
-    public void Dropdown (int index)
+    public void Dropdown(int index)
     {
-        if(index == 0) 
+        if(index == 0) {
             imageField.GetComponent<Image>().sprite = soriteArray[0];
-
-        else if (index == 1)
+            descriptionText.text = descriptions[0];
+        }
+        else if (index == 1) {
             imageField.GetComponent<Image>().sprite = soriteArray[1];
-
-        else if (index == 2)
+            descriptionText.text = descriptions[1];
+        }
+        else if (index == 2) {
             imageField.GetComponent<Image>().sprite = soriteArray[2];
+            descriptionText.text = descriptions[2];
+        }
     }
 }
