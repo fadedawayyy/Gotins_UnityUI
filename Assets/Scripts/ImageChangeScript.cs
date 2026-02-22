c3e002396d74395c9456d0934576e401c030381d
fadedawayyy <gergot.gg@gmail.com>
2026-02-22 22:42:46 +0200

    Uztaisiju dropdown

---
+++ Assets/Scripts/ImageChangeScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChangeScript : MonoBehaviour
+{
+    public GameObject imageField;
+    public Sprite[] soriteArray;
+
+    public void Dropdown (int index)
+    {
+        if(index == 0) 
+            imageField.GetComponent<Image>().sprite = soriteArray[0];
+
+        else if (index == 1)
+            imageField.GetComponent<Image>().sprite = soriteArray[1];
+
+        else if (index == 2)
+            imageField.GetComponent<Image>().sprite = soriteArray[2];
+    }
+}
