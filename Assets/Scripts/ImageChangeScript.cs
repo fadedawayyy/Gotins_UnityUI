d51d7e14e2c1fea8212bed3fe1d212adcaf31fc5
fadedawayyy <gergot.gg@gmail.com>
2026-02-23 17:30:27 +0200

    Uztaisiju Toggle Script

--- Assets/Scripts/ImageChangeScript.cs
+++ Assets/Scripts/ImageChangeScript.cs
@@ -5,6 +5,22 @@ using UnityEngine.UI;
 
 public class ImageChangeScript : MonoBehaviour
 {
+    public GameObject LeatherHelmet;
+    public GameObject LeatherChestplate;
+    public GameObject LeatherLeggins;
+    public GameObject LeatherBoots;
+    public GameObject IronHelmet;
+    public GameObject IronChestplate;
+    public GameObject IronLeggins;
+    public GameObject IronBoots;
+    public GameObject GoldHelmet;
+    public GameObject GoldChestplate;
+    public GameObject GoldLeggins;
+    public GameObject GoldBoots;
+    public GameObject DiamondHelmet;
+    public GameObject DiamondChestplate;
+    public GameObject DiamondLeggins;
+    public GameObject DiamondBoots;
     public GameObject imageField;
     public Text descriptionText;
     public Sprite[] soriteArray;
@@ -42,5 +58,38 @@ public class ImageChangeScript : MonoBehaviour
             imageField.GetComponent<Image>().sprite = soriteArray[2];
             descriptionText.text = descriptions[2];
         }
+    }
+    
+    public void ToggleLeather(bool value)
+    {
+        LeatherHelmet.SetActive(value);
+        LeatherChestplate.SetActive(value);
+        LeatherLeggins.SetActive(value);
+        LeatherBoots.SetActive(value);
+
+    }
+    public void ToggleIron(bool value)
+    {
+        IronHelmet.SetActive(value);
+        IronChestplate.SetActive(value);
+        IronLeggins.SetActive(value);
+        IronBoots.SetActive(value);
+
+    }
+    public void ToggleGold(bool value)
+    {
+        GoldHelmet.SetActive(value);
+        GoldChestplate.SetActive(value);
+        GoldLeggins.SetActive(value);
+        GoldBoots.SetActive(value);
+
+    }
+    public void ToggleDiamond(bool value)
+    {
+        DiamondHelmet.SetActive(value);
+        DiamondChestplate.SetActive(value);
+        DiamondLeggins.SetActive(value);
+        DiamondBoots.SetActive(value);
+
     }
 }
